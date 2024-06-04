7d941b775edf4925484cb719eccb453de899a807
takatsunoitsuki <itsukitakatsuno@gmail.com>
2024-06-05 02:33:07 +0900

    また戻す

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     host: true,
     port: 3000,
   },
-  base: './',
+  base: process.env.GITHUB_PAGES ? 'chess-alpha' : './',
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
   },
