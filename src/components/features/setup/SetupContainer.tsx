5719b2573e2a2b7b578419361c605925d7719405
Hanse Kim <procrustes5@gmail.com>
2024-06-05 23:28:30 +0900

    不要なコード削除

--- src/components/features/setup/SetupContainer.tsx
+++ src/components/features/setup/SetupContainer.tsx
@@ -7,12 +7,6 @@ import { appURL } from "@/config/url";
 import { useAlert } from "@/hooks/alert";
 
 export function SetupContainer() {
-  const [isRuleBookOpen, setIsRuleBookOpen] = useState(false);
-
-  const [modalMode, setModalMode] = useState<
-    "cpu" | "prevData" | "online" | undefined
-  >(undefined);
-
   const [playerColor, setPlayerColor] = useState<"black" | "white" | undefined>(
     undefined,
   );
@@ -20,47 +14,6 @@ export function SetupContainer() {
   const navigate = useNavigate();
 
   const { showError } = useAlert();
-
-  const gameDataAPI = useGameDataAPI();
-
-  const handleClickCPUBattle = async () => {
-    try {
-      const latest = await gameDataAPI.findLatest();
-      if (latest == null) {
-        setModalMode("cpu");
-      } else {
-        setModalMode("prevData");
-        setPlayerColor(latest.playerColor);
-      }
-    } catch (e) {
-      console.error(e);
-      showError({
-        message: "エラーが発生しました。もう一度お試しください。",
-      });
-    }
-  };
-
-  const handleClickOnlineBattle = () => {
-    // TODO: Implement online battle
-    console.log("clicked online battle");
-  };
-
-  const handleClickRule = () => {
-    setIsRuleBookOpen(true);
-  };
-
-  const handleClickHistory = () => {
-    // TODO: Implement history
-    console.log("clicked history");
-  };
-
-  const handleCloseRuleBook = () => {
-    setIsRuleBookOpen(false);
-  };
-
-  const handleCloseModal = () => {
-    setModalMode(undefined);
-  };
 
   const handleClickPlayerColor = (color: "black" | "white" | "random") => {
     if (color === "random") {
@@ -72,30 +25,18 @@ export function SetupContainer() {
     }
   };
 
-  const handleContinuePrevData = async (isContinue: boolean) => {
-    if (isContinue) {
-      navigate(`${appURL.game}?player=${playerColor}`);
-    } else {
-      try {
-        await gameDataAPI.delete();
-      } catch (e) {
-        console.error(e);
-        showError({
-          message: "エラーが発生しました。もう一度お試しください。",
-        });
-      }
-      setModalMode("cpu");
-    }
-  };
-
   const handleBackHome = () => {
     navigate(`${appURL.home}`);
   };
 
+  const handleStart = () => {
+    navigate(`${appURL.playerRole}`);
+  }
+
   return (
     <SetupPresenter
-      handleClickCPUBattle={handleClickCPUBattle}
       handleBackHome={handleBackHome}
+      handleStart={handleStart}
     />
   );
 }
