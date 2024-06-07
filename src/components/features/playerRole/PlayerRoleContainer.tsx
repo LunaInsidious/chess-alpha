be619ce568b50cb11ece82cf8acef103b39e7eab
伊藤史人 <ittak@MacBook-Pro-2.local>
2024-06-07 15:05:08 +0900

    fix/comment

--- src/components/features/playerRole/PlayerRoleContainer.tsx
+++ src/components/features/playerRole/PlayerRoleContainer.tsx
@@ -1,42 +1,49 @@
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
+import { generateRandomNumber } from "@/adapters/util/mockutil";
 import { PlayerRolePresenter } from "@/components/features/playerRole/PlayerRolePresenter";
 import { appURL } from "@/config/url";
 import { useAlert } from "@/hooks/alert";
-
-const shuffleArray = (array: string[]): string[] => {
-  const shuffledArray = [...array];
-  for (let i = shuffledArray.length - 1; i > 0; i -= 1) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-  }
-  return shuffledArray;
-};
-
-const assignRoles = (players: string[]) => {
-  const roles = Array(players.length).fill("市民");
-  if (players.length > 0) {
-    roles[0] = "人狼";
-  }
-  return shuffleArray(roles);
-};
 
 export function PlayerRoleContainer() {
   const navigate = useNavigate();
   const location = useLocation();
   const { showError } = useAlert();
 
+  const shuffleArray = (array: string[]): string[] => {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i -= 1) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    return shuffledArray;
+  };
+
+  const assignRoles = (players: string[]) => {
+    const roles = Array(players.length).fill("市民");
+    const randomIndex = generateRandomNumber(players.length - 1);
+    if (roles.length > randomIndex) {
+      roles[randomIndex] = "人狼";
+      return shuffleArray(roles);
+    }
+
+    throw new Error("人狼の人数がプレイヤー数を超えています。");
+    };
+
   const queryParams = new URLSearchParams(location.search);
-  // const color = queryParams.get("color");
+
   const playersQuery = queryParams.get("players");
 
   const players =
     playersQuery !== null
       ? playersQuery
           .split(",")
-          .map((player) => player.trim())
           .filter((player) => player !== "")
+          .map((player) => decodeURIComponent(player))
       : [];
 
   const [roles, setRoles] = useState<string[]>([]);
@@ -47,11 +54,11 @@ export function PlayerRoleContainer() {
       showError({
         message: "プレイヤーが指定されていません。",
       });
-      navigate(appURL.home);
+      navigate(appURL.playerSetup);
     } else {
       setRoles(assignRoles(players));
     }
-  }, [players, navigate, showError]);
+  }, [players]);
 
   const handleNextPlayer = () => {
     if (currentPlayerIndex < players.length - 1) {
