affd7d09961d6b9e8f3e9199d15b1a78a08f7281
伊藤史人 <ittak@MacBook-Pro-2.local>
2024-06-07 16:32:49 +0900

    add/mockutil/

--- src/adapters/util/mockutil.ts
+++ src/adapters/util/mockutil.ts
@@ -13,3 +13,7 @@ export function generateRandomString(maxLength: number) {
   }
   return result;
 }
+
+export function generateRandomNumber(maxNumber: number) {
+  return Math.floor(Math.random() * maxNumber); // Generate a random length between 0 and maxNumber
+}
