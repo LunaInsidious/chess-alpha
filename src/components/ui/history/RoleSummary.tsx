034e5b069748a16f47fae0b0f3a1ba1ed09319c1
Hanse Kim <procrustes5@gmail.com>
2024-06-16 18:16:32 +0900

    役職別ゲーム数表示

--- src/components/ui/history/RoleSummary.tsx
+++ src/components/ui/history/RoleSummary.tsx
@@ -2,12 +2,24 @@ import { HistoryCard } from "@/components/ui/HistoryCard";
 
 export function RoleSummary(): JSX.Element {
   return (
-    <div className="flex justify-between gap-4">
-      <HistoryCard className="w-full">
-        <div className="flex-col">人狼</div>
+    <div className="flex justify-between gap-4 h-20">
+      <HistoryCard className="w-full flex items-center">
+        <div className="flex justify-between w-full">
+          <span className="font-black text-xl">人狼</span>
+          <div className="flex gap-2 items-end">
+            <span className="font-black text-2xl">6</span>
+            <span className="text-sm">ゲーム</span>
+          </div>
+        </div>
       </HistoryCard>
-      <HistoryCard className="w-full">
-        <div className="flex-col">市民</div>
+      <HistoryCard className="w-full flex items-center">
+        <div className="flex flex-row-reverse justify-between w-full">
+          <span className="font-black text-xl">市民</span>
+          <div className="flex gap-2 items-end">
+            <span className="font-black text-2xl">14</span>
+            <span className="text-sm">ゲーム</span>
+          </div>
+        </div>
       </HistoryCard>
     </div>
   );
