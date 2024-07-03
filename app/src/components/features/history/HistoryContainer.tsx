c3ea26dc4f910a304f3c69718f648b8efed1b6fe
Hanse Kim <procrustes5@gmail.com>
2024-07-03 22:45:51 +0900

    ホームに戻るボタン

--- app/src/components/features/history/HistoryContainer.tsx
+++ app/src/components/features/history/HistoryContainer.tsx
@@ -1,5 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
+import { appURL } from "@/config/url";
 import { HistoryPresenter } from "@/components/features/history/HistoryPresenter";
 
 export function HistoryContainer() {
-  return <HistoryPresenter />;
+  const navigate = useNavigate();
+  const handleBackHome = () => {
+    navigate(appURL.home);
+  };
+
+  return <HistoryPresenter 
+    handleClick={handleBackHome}
+  />;
 }
--- app/src/components/features/history/HistoryPresenter.tsx
+++ app/src/components/features/history/HistoryPresenter.tsx
@@ -1,15 +1,30 @@
+import { Button } from "@/components/ui/button/Button";
 import { HistoryDashboard } from "@/components/ui/history/HistoryDashboard";
 import { HistoryGraphWrapper } from "@/components/ui/history/HistoryGraphWrapper";
 import { HistoryHeader } from "@/components/ui/history/HistoryHeader";
 import { RoleSummary } from "@/components/ui/history/RoleSummary";
 
-export function HistoryPresenter(): JSX.Element {
+type Props = {
+  handleClick: () => void;
+};
+
+export function HistoryPresenter({handleClick}: Props): JSX.Element {
+  
   return (
     <div className="flex flex-col p-8 gap-6 w-full">
       <HistoryHeader />
       <HistoryGraphWrapper />
       <RoleSummary />
       <HistoryDashboard />
+      <div className="w-full flex justify-end">
+        <Button 
+          onClick={handleClick}
+          variant="primary"
+          className="w-20 h-10 md:w-20 md:h-12 md:text-l lg:text-xl"
+        >
+          戻る
+        </Button>
+      </div>
     </div>
   );
 }
